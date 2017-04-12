5572bbc0447667e9e4441a40eaa0de023a4403eb
Ray Holland <bravadomizzou@users.noreply.github.com>
2017-04-12 13:54:50 -0500

    Make endpoint publicly accessable

--- delta-404.php
+++ delta-404.php
@@ -16,37 +16,40 @@ add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'add_action_link
 
 function add_action_links ( $links ) {
 	$additionalLinks = array(
-	'<a href="' . admin_url( 'admin-ajax.php?action=delta404' ) . '">404 RSS Feed</a>',
+	'<a href="/?delta404">404 RSS Feed</a>',
 	);
 	return array_merge( $links, $additionalLinks );
 }
 
+add_action( 'init', 'delta404' );
 
-add_action( 'wp_ajax_delta404', function () {
-    global $wpdb;
+function delta404() {
+    if (isset($_GET['delta404'])) {
+        global $wpdb;
 
-    $items  = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}redirection_404" );
+        $items  = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}redirection_404" );
 
-    header( 'Content-type: text/xml; charset='.get_option( 'blog_charset' ), true );
-    echo '<?xml version="1.0" encoding="'.get_option( 'blog_charset' ).'"?'.">\r\n";
-    ?>
-	<rss version="2.0"
-		 xmlns:content="http://purl.org/rss/1.0/modules/content/"
-		 xmlns:wfw="http://wellformedweb.org/CommentAPI/"
-		 xmlns:dc="http://purl.org/dc/elements/1.1/">
-		<items>
-            <?php foreach ( (array) $items as $item ) : ?>
-				<item>
-					<id><?php echo esc_html( $item->id ); ?></id>
-					<created><?php echo esc_html( $item->created ); ?></created>
-					<url><?php echo esc_html( $item->url ); ?></url>
-					<agent><?php echo esc_html( $item->agent ); ?></agent>
-					<referrer><?php echo esc_html( $item->referrer ); ?></referrer>
-					<ip><?php echo esc_html( $item->ip ); ?></ip>
-				</item>
-            <?php endforeach; ?>
-		</items>
-	</rss>
-    <?php
-	wp_die();
-});
+        header( 'Content-type: text/xml; charset='.get_option( 'blog_charset' ), true );
+        echo '<?xml version="1.0" encoding="'.get_option( 'blog_charset' ).'"?'.">\r\n";
+        ?>
+        <rss version="2.0"
+             xmlns:content="http://purl.org/rss/1.0/modules/content/"
+             xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+             xmlns:dc="http://purl.org/dc/elements/1.1/">
+            <items>
+                <?php foreach ( (array) $items as $item ) : ?>
+                    <item>
+                        <id><?php echo esc_html( $item->id ); ?></id>
+                        <created><?php echo date( 'D, d M Y H:i:s +0000', $item->created ); ?></created>
+                        <url><?php echo esc_html( $item->url ); ?></url>
+                        <agent><?php echo esc_html( $item->agent ); ?></agent>
+                        <referrer><?php echo esc_html( $item->referrer ); ?></referrer>
+                        <ip><?php echo esc_html( $item->ip ); ?></ip>
+                    </item>
+                <?php endforeach; ?>
+            </items>
+        </rss>
+        <?php
+        die;
+    }
+}
